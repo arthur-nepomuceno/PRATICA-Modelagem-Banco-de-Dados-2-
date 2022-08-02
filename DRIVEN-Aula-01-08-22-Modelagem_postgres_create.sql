5e77c2c11ad3379d6fcb18888fd73c42decf4f7d
Arthur Nepomuceno <arth.nepomuceno@gmail.com>
2022-08-01 21:32:12 -0300

    tudo feito - tabela de endereços adicionada

--- DRIVEN-Aula-01-08-22-Modelagem_postgres_create.sql
+++ DRIVEN-Aula-01-08-22-Modelagem_postgres_create.sql
@@ -4,6 +4,7 @@ CREATE TABLE "public.users" (
 	"email" varchar(255) NOT NULL UNIQUE,
 	"password" varchar(255) NOT NULL,
 	"cpf" integer NOT NULL UNIQUE,
+	"addressId" integer NOT NULL,
 	CONSTRAINT "users_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
@@ -43,6 +44,7 @@ CREATE TABLE "public.purchases" (
 	"totalValue" double NOT NULL,
 	"status" TEXT NOT NULL,
 	"date" DATE NOT NULL,
+	"addressId" integer NOT NULL,
 	CONSTRAINT "purchases_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
@@ -62,6 +64,52 @@ CREATE TABLE "public.productPhotos" (
 
 
 
+CREATE TABLE "public.addresses" (
+	"id" serial NOT NULL,
+	"street" TEXT NOT NULL,
+	"number" integer NOT NULL,
+	"complement" TEXT,
+	"cityId" integer NOT NULL,
+	"stateId" integer NOT NULL,
+	"countryId" integer NOT NULL,
+	CONSTRAINT "addresses_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public.cities" (
+	"id" serial NOT NULL,
+	"name" integer NOT NULL UNIQUE,
+	CONSTRAINT "cities_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public.countries" (
+	"id" serial NOT NULL,
+	"name" integer NOT NULL UNIQUE,
+	CONSTRAINT "countries_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public.states" (
+	"id" serial NOT NULL,
+	"name" integer NOT NULL UNIQUE,
+	CONSTRAINT "states_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+ALTER TABLE "users" ADD CONSTRAINT "users_fk0" FOREIGN KEY ("addressId") REFERENCES "addresses"("id");
 
 ALTER TABLE "products" ADD CONSTRAINT "products_fk0" FOREIGN KEY ("mainPhotoId") REFERENCES "productPhotos"("id");
 ALTER TABLE "products" ADD CONSTRAINT "products_fk1" FOREIGN KEY ("categoryId") REFERENCES "productCategories"("id");
@@ -69,8 +117,20 @@ ALTER TABLE "products" ADD CONSTRAINT "products_fk1" FOREIGN KEY ("categoryId") 
 
 ALTER TABLE "purchases" ADD CONSTRAINT "purchases_fk0" FOREIGN KEY ("userId") REFERENCES "users"("id");
 ALTER TABLE "purchases" ADD CONSTRAINT "purchases_fk1" FOREIGN KEY ("productId") REFERENCES "products"("id");
+ALTER TABLE "purchases" ADD CONSTRAINT "purchases_fk2" FOREIGN KEY ("addressId") REFERENCES "addresses"("id");
 
 ALTER TABLE "productPhotos" ADD CONSTRAINT "productPhotos_fk0" FOREIGN KEY ("categoryId") REFERENCES "productCategories"("id");
+
+ALTER TABLE "addresses" ADD CONSTRAINT "addresses_fk0" FOREIGN KEY ("cityId") REFERENCES "cities"("id");
+ALTER TABLE "addresses" ADD CONSTRAINT "addresses_fk1" FOREIGN KEY ("stateId") REFERENCES "states"("id");
+ALTER TABLE "addresses" ADD CONSTRAINT "addresses_fk2" FOREIGN KEY ("countryId") REFERENCES "countries"("id");
+
+
+
+
+
+
+
 
 
 
